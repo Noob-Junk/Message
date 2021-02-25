5f5ca377ea2f1455fc58dc2081ce8efcec65afe0
Noob-Junk <77061416+Noob-Junk@users.noreply.github.com>
2021-02-25 10:42:10 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
   $hostname = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "chatapp";
+  $username = "id16242322_splakabase";
+  $password = "TCV=\]-ah]R3u>{M";
+  $dbname = "id16242322_splakadern";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
